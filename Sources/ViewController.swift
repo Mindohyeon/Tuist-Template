dc6251f4840cfa656f60119216c2411f6fb04de0
Mindohyeon <min50875@naver.com>
2022-10-21 15:40:19 +0900

    :sparkles: Add SceneDelegate

---
+++ Sources/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  TuistTest
+//
+//  Created by 민도현 on 2022/10/20.
+//  Copyright © 2022 com.dohyeon. All rights reserved.
+//
+
+import UIKit
+
+class firstViewController: UIViewController {
+    override func viewDidLoad() {
+        view.backgroundColor = .blue
+        print("hi")
+    }
+}
