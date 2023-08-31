a40cdb5c875fd38f0f685d079c9a08ad6e67a0cd
Mindohyeon <min50875@naver.com>
2023-08-31 20:52:45 +0900

    Tests 로 인한 에러 해결

--- Project.swift
+++ Project.swift
@@ -4,10 +4,12 @@ import ProjectDescriptionHelpers
 let project = Project.makeModule(
     name: Environments.appName,
     organizationName: Environments.organizationName,
+    platform: .iOS,
     product: .app,
     dependencies: [
         .SPM.SnapKit,
         .SPM.Then
     ],
-    resources: ["Resources/**"]
+    resources: ["Resources/**"],
+    infoPlist: .file(path: "Support/Info.plist")
 )
